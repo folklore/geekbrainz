18d3fedc41e8ce22769457e1fcf7f0db21fca0a3
Gitea <gitea@fake.local>
2022-08-24 01:36:04 +0500

    Урок 6

---
+++ Урок 6/Задача 41/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+string answer = "yes";
+double[] numbers = new double[] {};
+int greater_than_0_count = 0;
+int index = 0;
+
+while(answer == "yes" || answer == "y") {
+    Console.Write("Введите число: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+
+    if(number > 0) { greater_than_0_count++; };
+
+    Array.Resize(ref numbers, index + 1);
+    numbers[index] = number;
+
+    Console.Write("Продолжить ввод? [y]es/[n]o: ");
+    answer = Console.ReadLine();
+
+    index++;
+}
+
+Console.WriteLine($"[{String.Join(", ", numbers)}] -> {greater_than_0_count}");
---
+++ Урок 6/Задача 43/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Уравнения прямых: y = k1 * x + b1; y = k2 * x + b2;");
+Console.WriteLine();
+
+Console.Write("Введите k1 для первого уравнения: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1 для первого уравнения: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите k2 для второго уравнения: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2 для второго уравнения: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+
+Console.WriteLine();
+Console.WriteLine($"y = {k1} * x + {b1}; y = {k2} * x + {b2}");
+Console.WriteLine($"Точка пересечения двух прямых ({x}; {y})");
